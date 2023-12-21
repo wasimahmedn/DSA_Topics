27b95eb59861295432d871a19b92afcb457c2f22
Wasim Ahmed N <wasimahmedn@gmail.com>
2023-12-22 02:10:37 +0530

    Pushing the standalone upper and lower bound modules

--- BoundSearch/lower_bound.py
+++ BoundSearch/lower_bound.py
@@ -7,3 +7,8 @@ def lower_bound(lst:list[int],x:int)->int:
         else:
             lo=mid+1
     return lo
+
+
+if __name__=="__main__":
+    lst=[1,2,2,2,2,2,2,2,3,10,11,19,20,22]
+    print(lower_bound(lst,2))# The output will be 1 as the lower bound of 2 in lst in 1.
--- BoundSearch/upper_bound.py
+++ BoundSearch/upper_bound.py
@@ -7,3 +7,6 @@ def upper_bound(lst:list[int],x:int)->int:
         else:
             lo=mid+1
     return lo
+if __name__=="__main__":
+    lst=[1,2,2,2,2,2,3,4,5,10,11]
+    print(upper_bound(lst,2))#output will be 6 as it has the upper bound index 5 and next element can be inserted at 6th index.
